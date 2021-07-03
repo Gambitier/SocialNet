bd04063044f4296341bcadbea58d782595935dc0
gambitier <csegambitier@gmail.com>
2021-07-03 11:16:34 +0530

    Add project files.

---
+++ UserManagement/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagement.AuthManager;
+using UserManagement.InputModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly JWTAuthenticationManager jwtAuthenticationManager;
+
+        public LoginController(JWTAuthenticationManager jWTAuthenticationManager)
+        {
+            jwtAuthenticationManager = jWTAuthenticationManager;
+        }
+
+        // POST api/<AuthorizationController>
+        [HttpPost]
+        public IActionResult Login([FromBody] UserCredential userCreds)
+        {
+            var token = jwtAuthenticationManager.Authenticate(userCreds.UserName, userCreds.Password);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(token);
+        }
+    }
+}
---
+++ UserManagement/InputModels/UserCreds.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagement.InputModels
+{
+    public class UserCredential
+    {
+        public string Password { get; set; }
+        public string UserName { get; set; }
+    }
+}
