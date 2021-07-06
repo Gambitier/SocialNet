64784f3f243783699c9c786a38ae9a8a56be220c
gambitier <csegambitier@gmail.com>
2021-07-06 17:24:47 +0530

    add doc comment

--- UserManagement.Persistence/DBConfiguration/DbConfig.cs
+++ UserManagement.Persistence/DBConfiguration/DbConfig.cs
@@ -1,5 +1,9 @@
 ﻿namespace UserManagement.Persistence.DBConfiguration
 {
+    /// <summary>
+    /// these configs needs to be initialized with user-secrets
+    /// ref: https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-5.0&tabs=windows
+    /// </summary>
     public class DbConfig
     {
         public string Database_Name { get; set; }
--- UserManagement.Services/SendGridConfigs/AuthMessageSenderOptions.cs
+++ UserManagement.Services/SendGridConfigs/AuthMessageSenderOptions.cs
@@ -1,5 +1,9 @@
 ﻿namespace UserManagement.Services.SendGridConfigs
 {
+    /// <summary>
+    /// these configs needs to be initialized with user-secrets
+    /// ref: https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-5.0&tabs=windows
+    /// </summary>
     public class AuthMessageSenderOptions
     {
         public string SendGridUser { get; set; }
